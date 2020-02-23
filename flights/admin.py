10347738f22c52ddd34a4c0715340f6bea1cdc50
sarataiseer <sarataiseer97@gmail.com>
2020-02-23 14:39:42 +0200

    Rest Task1

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Flight,Booking
+admin.site.register(Flight)
+admin.site.register(Booking)
 # Register your models here.
--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
 from .models import Flight, Booking
+from rest_framework.generics import ListAPIView
+from .serializers import ListSerializer,ListSerializerbooking
+from datetime import datetime
+
+
+
+class ListView(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = ListSerializer
+
+class Booking(ListAPIView):
+    queryset = Booking.objects.filter(date__gt=datetime.today())
+    serializer_class = ListSerializerbooking
